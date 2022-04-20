276d5a9a69ac5fdab834b18a52d773f35c633d11
rj1337 <mathesonroland@gmail.com>
2022-04-20 15:16:57 +0900

    add hash routing, style guild display

--- src/components/GuildsDisplay.jsx
+++ src/components/GuildsDisplay.jsx
@@ -1,36 +1,56 @@
-import { Card, Space } from "antd";
+import { Card, Space, Spin, Col, Row, Image } from "antd";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const { Meta } = Card;
 
 export default function GuildsDisplay(props) {
   const { guilds } = props;
+
+  if (guilds.length === 0) {
+    return <Spin />;
+  }
+
+  const style = {
+    padding: "10%",
+    textAlign: "center",
+    display: "inline-flex",
+    alignItems: "center",
+    width: 320,
+    height: 400,
+  };
+
   return (
-    <Space
-      direction="horizontal"
-      style={{ width: "100%", justifyContent: "center" }}
-    >
-      {guilds.map((guild, index) => (
-        <NavLink key={`${guild[1]}`} to={`/guild/${index}`}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img
-                style={{ width: 200, height: 200 }}
-                alt={`${guild[1]}'s logo`}
-                src={guild[2]}
-              />
-            }
-          >
-            <Meta
-              title={`${guild[1]}`}
-              description={`${Number(guild[4]._hex)} members`}
-            />
-          </Card>
-        </NavLink>
-      ))}
-    </Space>
+    <>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        {guilds.map((guild, index) => (
+          <>
+            <Col>
+              <NavLink key={`${guild[1]}`} to={`/guild/${index}`}>
+                <Card style={style} hoverable>
+                  <Image
+                    style={{
+                      width: 200,
+                      height: 200,
+                      borderRadius: "50%",
+                    }}
+                    preview={false}
+                    alt={`${guild[1]}'s logo`}
+                    src={guild[2]}
+                  />
+
+                  <Meta
+                    title={`${guild[1]}`}
+                    description={`${Number(guild[4]._hex)} members`}
+                  />
+                </Card>
+              </NavLink>
+            </Col>
+          </>
+        ))}
+      </Row>
+    </>
   );
 }
